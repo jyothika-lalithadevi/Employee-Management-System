320e9045812f5e45457c89faf02d74bbd850cdb7
Jyothika Lalithadevi Dasari <jyothikalalithadevidasari@gmail.com>
2026-09-24 13:46:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template,request,redirect,session, make_response
 import sqlite3
 from werkzeug.security import generate_password_hash,check_password_hash
-app=Flask(__name__)
+app=Flask(__name__
+         template_folder="Templates")
 app.secret_key="employee-management-secret-key"
 def get_database_connection():
    connection=sqlite3.connect("users.db")
